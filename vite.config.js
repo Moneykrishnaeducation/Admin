1f18de21a647a53b5b71c268333a9588df09c6c5
Twoface002 <tamizharasan344@gmail.com>
2025-12-12 13:52:31 +0530

    compress

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import compression from 'vite-plugin-compression'
 
 // ADMIN CONFIG
 const ADMIN = {
   base: "/static/admin/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({ algorithm: 'gzip', ext: '.gz', threshold: 1024 }),
+    compression({ algorithm: 'brotliCompress', ext: '.br', threshold: 1024 })
+  ],
   build: {
     outDir: "../static/admin",
     emptyOutDir: true,
@@ -19,7 +24,11 @@ const ADMIN = {
 // MANAGER CONFIG
 const MANAGER = {
   base: "/static/admin/manager/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({ algorithm: 'gzip', ext: '.gz', threshold: 1024 }),
+    compression({ algorithm: 'brotliCompress', ext: '.br', threshold: 1024 })
+  ],
   build: {
     outDir: "../static/admin/manager",
     emptyOutDir: true,
