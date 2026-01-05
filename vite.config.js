2481b857edb84f5d133d3782d5eacddbc76574a9
Twoface002 <tamizharasan344@gmail.com>
2026-01-05 13:05:50 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import compression from 'vite-plugin-compression';
+import compression from 'vite-plugin-compression'
 
 // ADMIN CONFIG
 const ADMIN = {
   base: "/static/admin/",
   plugins: [
     react(),
-    tailwindcss(),
     compression({ algorithm: 'gzip', ext: '.gz', threshold: 500 * 1024, deleteOriginalAssets: true }), // Increase threshold to 500 KB
     compression({ algorithm: 'brotliCompress', ext: '.br', threshold: 500 * 1024, deleteOriginalAssets: true }) // Increase threshold to 500 KB
   ],
@@ -17,18 +15,7 @@ const ADMIN = {
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        index: "./index.html",  // Main entry point for Admin
-      },
-      output: {
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash][extname]',
-        // Optionally, split vendor code into its own chunk for caching
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
+        index: "./index.html",
       },
     },
   },
@@ -39,7 +26,6 @@ const MANAGER = {
   base: "/static/admin/manager/",
   plugins: [
     react(),
-    tailwindcss(), // Add TailwindCSS to the MANAGER config as well
     compression({ algorithm: 'gzip', ext: '.gz', threshold: 500 * 1024, deleteOriginalAssets: true }), // Increase threshold to 500 KB
     compression({ algorithm: 'brotliCompress', ext: '.br', threshold: 500 * 1024, deleteOriginalAssets: true }) // Increase threshold to 500 KB
   ],
@@ -48,24 +34,12 @@ const MANAGER = {
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        manager: "./main.html",  // Main entry point for Manager
-      },
-      output: {
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash][extname]',
-        // Optionally, split vendor code into its own chunk for caching
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
+        manager: "./main.html",
       },
     },
   },
 };
 
 export default defineConfig(({ mode }) => {
-  // Return the appropriate config based on the mode (manager or admin)
   return mode === "manager" ? MANAGER : ADMIN;
 });
