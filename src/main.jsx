857a038afaba6b797ec54df152f07f619966a7d8
moneykrishnaeducation <moneykrishnaeducation@gmail.com>
2026-01-12 17:02:51 +0530

    theme proviode the main page

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import NotificationProvider from './context/NotificationProvider'
+import { ThemeProvider } from './context/ThemeContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NotificationProvider>
-      <App />
-    </NotificationProvider>
+    <ThemeProvider>
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
 
